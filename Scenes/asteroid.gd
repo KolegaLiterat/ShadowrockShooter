e2437642494635a33d008d64adb1beaeb41308d5
Adrian "Literat" Jaworek <adrian@kolegaliterat.pl>
2024-08-17 16:54:15 +0200

    Added asteroids cleanup

--- Scenes/asteroid.gd
+++ Scenes/asteroid.gd
@@ -1,9 +1,16 @@
 extends RigidBody2D
 
-# Called when the node enters the scene tree for the first time.
+@onready var screen_size = get_viewport().get_visible_rect().size
+
 func _ready():
 	pass # Replace with function body.
-
+ 
+func _physics_process(delta): 
+	if position.x + 10 > screen_size.x or position.x - 10 < 0:
+		queue_free()
+	
+	if position.y + 10 > screen_size.y or position.y - 10 < 0:
+		queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
