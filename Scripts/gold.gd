aea06672c86911a20465856c833d396010923f7c
Adrian "Literat" Jaworek <adrian@kolegaliterat.pl>
2024-08-18 12:32:11 +0200

    Fixed sound effect for gold catching

--- Scripts/gold.gd
+++ Scripts/gold.gd
@@ -12,12 +12,20 @@ func _process(delta):
 	
 func _physics_process(delta):
 	if position.x + 10 > screen_size.x or position.x - 10 < 0:
-		queue_free()
+		if $"Gold Catch".playing == false:
+			queue_free()
 	
 	if position.y + 10 > screen_size.y or position.y - 10 < 0:
-		queue_free()
+		if $"Gold Catch".playing == false:
+			queue_free()
 
 func _on_body_entered(body: Node2D):
 	if body.is_in_group("Player"):
 		Globals.Score += 1
-		queue_free()  # Replace with function body.
+		$"Gold Catch".play()
+		visible = false
+		$Collision.disabled = true
+		
+		
+func _on_gold_catch_finished():
+	queue_free()
